637ea9bc422d1b22ac0fac511c763a5cf82ceaa0
NYuya <noooochan5@gmail.com>
2020-06-12 17:47:16 +0900

    5-47

--- lesson.py
+++ lesson.py
@@ -1,3 +1,4 @@
+# 4-18
 r = [1, 2, 3, 4, 5, 1, 2, 3]
 print(r.index(3))
 print(r.index(3, 3))
@@ -16,10 +17,83 @@ print(r)
 
 
 s = 'My name is Mike'
-to_split = s.split(' ')  # 空白がある事でスプリットで分けてくれる
+
+# 空白がある事でスプリットで分けてくれる
+to_split = s.split(' ')
 print(to_split)
 
-x = ' '.join(to_split)  # 空白で繋いでくれる
+# 空白で繋いでくれる
+x = ' '.join(to_split)
 print(x)
 
-print(help(list))  # メソッド一覧表示
+# メソッド一覧表示
+# print(help(list))
+
+# 5-33
+x = 11
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('10')
+else:
+    print('positive')
+
+
+a = 5
+b = 10
+
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is positive')
+
+
+print('Lesson 5-39')
+# count = 0
+
+# while count < 5:
+#     print(count)
+#     count += 1
+
+count = 0
+
+while True:
+    if count >= 5:
+        break
+
+    if count == 2:
+        count += 1
+        continue
+
+    print(count)
+    count += 1
+
+
+print('Lesson 5-40')
+count = 0
+
+while count < 5:
+    if count == 1:
+        break
+    print(count)
+    count += 1
+else:
+    print('done')
+
+
+print('Lesson 5-41')
+while True:
+    word = input('Enter:')
+    num = int(word)
+    if num == 100:
+        break
+    print('next')
+
+
+print('Lesson 5-42')
+some_list = [1, 2, 3, 4, 5]
+
+i = 0
