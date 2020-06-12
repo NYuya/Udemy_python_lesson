e9d2b5bad7a2799a076a7aeaebc0c5ce65be215a
NYuya <noooochan5@gmail.com>
2020-06-12 13:44:49 +0900

    4-18

--- lesson.py
+++ lesson.py
@@ -7,3 +7,19 @@ print(r.count(3))
 if 5 in r:
     print("exist")
 
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+
+
+s = 'My name is Mike'
+to_split = s.split(' ')  # 空白がある事でスプリットで分けてくれる
+print(to_split)
+
+x = ' '.join(to_split)  # 空白で繋いでくれる
+print(x)
+
+print(help(list))  # メソッド一覧表示
