bb72bba19b85f0279136a4ffa8e253e4eca283c0
NYuya <noooochan5@gmail.com>
2020-06-13 23:56:59 +0900

    5-56

--- lesson5-53.py
+++ lesson5-53.py
@@ -16,3 +16,45 @@ d = {
 }
 
 menu(**d)
+
+
+def menu(food, *args, **kwargs):  # 「＊」の順番、小さい順に並べる
+    print(food)
+    print(args)
+    print(kwargs)
+
+
+menu('banana', 'apple', 'orange', entree='beef', drink='coffee')
+
+
+print('Lesson 5-55')
+
+
+def outer(a, b):
+
+    def plus(c, d):
+        return c + d
+
+    r1 = plus(a, b)
+    r2 = plus(a, b)
+    print(r1 + r2)
+
+
+outer(1, 2)
+
+
+print('Lesson 5-56')
+
+
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
+
+
+ca1 = circle_area_func(3.14)
+ca2 = circle_area_func(3.141592)
+
+print(ca1(10))
+print(ca2(10))
