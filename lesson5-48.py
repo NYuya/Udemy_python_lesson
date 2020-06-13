4ebf0c1fe46d264758cfd6b78d022d1a38213835
NYuya <noooochan5@gmail.com>
2020-06-13 22:22:22 +0900

    5-51

--- lesson5-48.py
+++ lesson5-48.py
@@ -21,3 +21,20 @@ result = what_is_this('red')
 # result = what_is_this('green')
 # result = what_is_this('yellow')
 print(result)
+
+
+print('Lesson 5-50')
+
+
+def test_func(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+
+r = test_func(100)
+print(r)
+
+r = test_func(100)
+print(r)
