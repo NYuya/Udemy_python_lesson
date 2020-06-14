f183c6b26c35f201ee1319cd84cedff56e906c63
NYuya <noooochan5@gmail.com>
2020-06-14 23:20:40 +0900

    5-62

--- lesson5-61.py
+++ lesson5-61.py
@@ -10,3 +10,16 @@ print(d)
 
 d = {x: y for x, y in zip(w, f)}
 print(d)
+
+
+print('Lesson 5-62')
+s = set()
+
+for i in range(10):
+    if i % 2 == 0:
+        s.add(i)
+
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
