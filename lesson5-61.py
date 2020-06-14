998c8ba5c74d37ebffac6b5a223fa67c30738481
NYuya <noooochan5@gmail.com>
2020-06-14 23:55:16 +0900

    5-64

--- lesson5-61.py
+++ lesson5-61.py
@@ -23,3 +23,50 @@ print(s)
 
 s = {i for i in range(10) if i % 2 == 0}
 print(s)
+
+
+print('Lesson 5-63')
+
+
+def g():
+    for i in range(10):
+        yield i
+
+
+g = g()
+
+g = (i for i in range(10) if i % 2 == 0)
+print(type(g))
+print(g)
+for x in g:
+    print(x)
+
+
+print('Lesson 5-63')
+animal = 'cat'
+
+
+def f():
+    # print(animal)
+    animal = 'dog'
+    print('after', animal)
+
+
+f()
+print('globa:', animal)
+
+
+"""
+Test ###############
+"""
+animal = 'cat'
+
+
+def f():
+    """Test func doc"""
+    # print(f.__name__)
+    # print(f.__doc__)
+
+
+f()
+print('global:', globals())
