905f34c6347ea0f747dc9e70abc88da8b5069c76
NYuya <noooochan5@gmail.com>
2020-06-14 22:41:45 +0900

    5-60

--- lesson5-58.py
+++ lesson5-58.py
@@ -18,3 +18,65 @@ def change_words(words, func):
 
 change_words(l, lambda word: word.capitalize())
 change_words(l, lambda word: word.lower())
+
+
+print('Lesson 5-59')
+l = ['Good morning', 'Good afternoon', 'Good night']
+
+for i in l:
+    print(i)
+
+print("#############")
+
+
+def counter(num=10):
+    for _ in range(num):
+        yield 'run'
+
+
+def greeting():
+    yield 'Good morning'
+    yield 'Good afternoon'
+    yield 'Good night'
+
+
+# for g in greeting():
+#     print(g)
+
+g = greeting()
+c = counter()
+print(next(g))
+print("@@@@")
+print(next(c))
+
+print(next(g))
+print("@@@@")
+print(next(c))
+print(next(c))
+
+print(next(g))
+
+
+print('Lesson 5-60')
+t = (1, 2, 3, 4, 5)
+t2 = (5, 6, 7, 8, 9, 10)
+
+r = []
+for i in t:
+    if i % 2 == 0:
+        r.append(i)
+print(r)
+
+r = [i for i in t if i % 2 == 0]
+print(r)
+
+
+r = []
+for i in t:
+    for j in t2:
+        r.append(i * j)
+print(r)
+
+# リスト内包表記は無駄に使ってはいけない
+r = [i * j for i in t for j in t2]
+print(r)
