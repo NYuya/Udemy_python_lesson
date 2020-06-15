547ee57d888b8a38e0736d081a587b79f5c25439
NYuya <noooochan5@gmail.com>
2020-06-15 23:40:43 +0900

    5-67

---
+++ lesson6-67.py
@@ -0,0 +1,14 @@
+import builtins
+print('Lesson 6-67')
+
+print('Lesson 6-73 組み込み関数')
+print(globals())
+builtins.print()
+
+ranking = {
+    "A": 100,
+    "B": 85,
+    "C": 95
+}
+
+print(sorted(ranking, key=ran))
